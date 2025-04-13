78460df374841c20f63e3adeb27597e12675b1f4
donishabduvaliyev <donishkratos413677@gmail.com>
2025-04-13 19:58:07 +0500

    login-route-fix

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,12 +1,9 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-const PrivateRoute = ({ children }) => {
+const PrivateRoute = ({ element }) => {
     const token = localStorage.getItem("token"); 
-    return token ? children : <Navigate to="/login" />;
-
- 
-      
+    return token ? element : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
